f4338d81bf2585df197d64ca2ec4e31be8c0fada
Kyosuke Takayama <loiseau@gmail.com>
2019-12-19 19:56:09 +0900

    Add verify() method

--- src/SWA/IdToken.php
+++ src/SWA/IdToken.php
@@ -2,6 +2,9 @@
 
 namespace SWA;
 
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use CoderCat\JWKToPEM\JWKConverter;
+
 final class IdToken
 {
     private $header;
@@ -11,23 +14,48 @@ final class IdToken
     public function __construct($id_token)
     {
         [ $header, $payload, $sign ] = explode('.', $id_token, 3);
-        $this->header  = base64_decode($header);
-        $this->payload = base64_decode($payload);
-        $this->sign    = base64_decode($sign);
+        $this->header  = $header;
+        $this->payload = $payload;
+        $this->sign    = $sign;
     }
 
     public function getHeader()
     {
-        return $this->header;
+        return json_decode($this->decode($this->header));
     }
 
     public function getPayload()
     {
-        return new \SWA\Payload($this->payload);
+        return new \SWA\Payload($this->decode($this->payload));
     }
 
     public function getSign()
     {
-        return $this->sign;
+        return $this->decode($this->sign);
+    }
+
+    public function verify()
+    {
+        $kid = $this->getHeader()->{'kid'};
+        $pem = null;
+
+        $keys = file_get_contents("https://appleid.apple.com/auth/keys");
+        $json = json_decode($keys, true);
+
+        foreach ($json['keys'] as $key) {
+            if ($kid == $key['kid']) {
+                $jwkConverter = new JWKConverter();
+                $pem = $jwkConverter->toPEM($key);
+                break;
+            }
+        }
+
+        $payload = $this->header . "." . $this->payload;
+        return (new \Lcobucci\JWT\Signature($this->getSign()))->verify(new Sha256(), $payload, $pem);
+    }
+
+    private function decode($str)
+    {
+        return base64_decode(strtr($str, '-_.', '+/='));
     }
 }
